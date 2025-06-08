5e28067ada3a73c46822c2b79431026a247f6b5b
Bandilesithole <bandilesithole61@gmail.com>
2025-06-08 17:44:37 +0200

    Add project files.

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,17 @@
+﻿public class OutputString
+{
+    public string ModifyString(string inputString)
+    {
+        string[] words = inputString.Split(' ');
+        return words[4] + " " + words[2] + " " + words[0].ToLower();
+    }
+
+    public static void Main()
+    {
+        var modify = new OutputString();
+        string result = modify.ModifyString("Yash is a javascript ninja");
+        Console.WriteLine(result);
+
+    }
+}
+
