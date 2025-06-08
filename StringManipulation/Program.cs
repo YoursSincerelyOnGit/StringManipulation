b1d8948f47c8a45fc3fb74e7186a87f4ee27d694
Bandilesithole <bandilesithole61@gmail.com>
2025-06-08 18:05:35 +0200

    added comments

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -2,15 +2,15 @@
 {
     public string ModifyString(string inputString)
     {
-        string[] words = inputString.Split(' ');
-        return words[4] + " " + words[2] + " " + words[0].ToLower();
+        string[] words = inputString.Split(' '); // breaks the string sentence in to an array of words
+        return words[4] + " " + words[2] + " " + words[0].ToLower(); // returns the conjoined 5th word, 3rd word and 1st word (in lower case)
     }
 
     public static void Main()
     {
-        var modify = new OutputString();
-        string result = modify.ModifyString("Yash is a javascript ninja");
-        Console.WriteLine(result);
+        var modify = new OutputString(); //creates an instance of the OutputString class
+        string result = modify.ModifyString("Yash is a javascript ninja"); // calls the ModifyString method with a sample string
+        Console.WriteLine(result); // displays the result on console
 
     }
 }
